11f83dfde66c6fadc6a197075e38e47a3f49db19
user <user@DESKTOP-GG8V4I4>
2019-02-26 20:59:26 +0900

    DB에 GPS값 넣기 완료

--- TripAdviserSemiProject/src/tripAdviser/admin/model/dao/TravelAdminDao.java
+++ TripAdviserSemiProject/src/tripAdviser/admin/model/dao/TravelAdminDao.java
@@ -200,7 +200,7 @@ public class TravelAdminDao {
 			pstmt.setString(5, tp.getTrvAddress());
 			pstmt.setString(6, tp.getTrvReview());
 			pstmt.setString(7, tp.getTrvSmallCtg());
-			
+			pstmt.setString(8, tp.getTrvGps());
 			
 			result=pstmt.executeUpdate();
 			
